b71595253dd52178e4bee2e09528276a2c70b794
leehaneul623 <dyj03216@naver.com>
2022-08-08 17:16:09 +0900

    article 시작

--- src/main/java/com/mysite/sbb/Controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/Controller/ArticleController.java
@@ -1,103 +1,23 @@
 package com.mysite.sbb.Controller;
 
 import com.mysite.sbb.Dao.Article;
-import com.mysite.sbb.Dao.User;
 import com.mysite.sbb.Repository.ArticleRepository;
-import com.mysite.sbb.Repository.UserRepository;
-import com.mysite.sbb.Ut.Ut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
     @Autowired
-    private ArticleRepository articleRepository;
-    @Autowired
-    private UserRepository userRepository;
+    ArticleRepository articleRepository;
 
-    // C ======================================
-    @RequestMapping("/doWrite")
-    @ResponseBody
-    private String doWrite(String title, String body){
-        if(Ut.empty(title)){
-            return "제목을 입력해주세요.";
-        }
-        if(Ut.empty(body)){
-            return "내용을 입력해주세요.";
-        }
-
-        Article article = new Article();
-        article.setTitle(title);
-        article.setBody(body);
-        article.setRegDate(LocalDateTime.now());
-        article.setUpdateDate(LocalDateTime.now());
-        User user = userRepository.findById(1L).get();
-
-        article.setUser(user);
-
-        articleRepository.save(article);
-
-        return "%d번 게시물 생성이 완료되었습니다.".formatted(article.getId());
-    }
-
-
-    // R ======================================
     @RequestMapping("/list")
     @ResponseBody
     public List<Article> showList(){
         return articleRepository.findAll();
-    }
-
-    @RequestMapping("/detail")
-    @ResponseBody
-    public Article showDetail(Long id){
-        Article article = articleRepository.findById(id).get();
-        return article;
-    }
-
-    // U ======================================
-    @RequestMapping("/doModify")
-    @ResponseBody
-   public String doModify(Long id, String title, String body){
-        if(id == null){
-            return "게시물이 없습니다.";
-        }
-        if(Ut.empty(title)){
-            return "제목을 입력해주세요.";
-        }
-        if(Ut.empty(body)){
-            return "내용을 입력해주세요.";
-        }
-        if(!articleRepository.existsById(id)){
-            return "게시물이 없습니다.";
-        }
-
-        Article article = articleRepository.findById(id).get();
-        article.setUpdateDate(LocalDateTime.now());
-        article.setTitle(title);
-        article.setBody(body);
-
-        articleRepository.save(article);
-
-        return "%번 게시물 수정이 완료되었습니다.".formatted(article.getId());
-    }
-    // D ======================================
-    @RequestMapping("/doDelete")
-    @ResponseBody
-    public String doDelete(Long id){
-        if(!articleRepository.existsById(id)){
-            return "%번 게시물이 없습니다.".formatted(id);
-        }
-
-        Article article = articleRepository.findById(id).get();
-        articleRepository.delete(article);
-
-        return "%d번 게시물을 삭제 했습니다.".formatted(id);
     }
 }
--- src/main/java/com/mysite/sbb/Dao/Article.java
+++ src/main/java/com/mysite/sbb/Dao/Article.java
@@ -3,7 +3,10 @@ package com.mysite.sbb.Dao;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,7 +20,5 @@ public class Article {
     private LocalDateTime updateDate;
     private String title;
     private String body;
-
-    @ManyToOne
-    private User user;
+    private Long user_id;
 }
